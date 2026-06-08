c8406caf1e0ca1c06b8bebed28a82d473d4b8f04
Rue/DarkSc0r <DarkSc0r500@gmail.com>
2026-06-08 19:51:12 -0400

    initial commit

---
+++ player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var speed := 48
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction != Vector2.ZERO:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
